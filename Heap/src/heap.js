5f6a3e2a7a66b33dd65126bcadb02c75f9030ba1
Nick Bottomley <nhbottomley@gmail.com>
2014-06-16 20:25:28 -0700

    Pull upstream spec changes

--- Heap/src/heap.js
+++ Heap/src/heap.js
@@ -1,27 +1,27 @@
-var Heap = function(comparator) {
+var Heap = function( comparator ) {
     this._data = [];
 };
 
 // Helper methods.
-Heap.prototype._leftChildOf = function(index) {
-    return 2*index + 1;
+Heap.prototype._leftChildOf = function( index ) {
+    return 2 * index + 1;
 };
-Heap.prototype._rightChildOf = function(index) {
-    return 2*index + 2;
+Heap.prototype._rightChildOf = function( index ) {
+    return 2 * index + 2;
 };
-Heap.prototype._parentOf = function(index) {
-    return index ? Math.floor((index - 1)/2) : undefined;
+Heap.prototype._parentOf = function( index ) {
+    return index ? Math.floor( (index - 1) / 2 ) : undefined;
 };
-Heap.prototype._hasElementAt = function(index) {
+Heap.prototype._hasElementAt = function( index ) {
     return typeof this._data[index] !== "undefined";
 };
-Heap.prototype._swap = function(index1, index2) {
+Heap.prototype._swap = function( index1, index2 ) {
     var temp = this._data[index1];
     this._data[index1] = this._data[index2];
     this._data[index2] = temp;
 };
 Heap.prototype.show = function() {
-    console.log(this._data);
+    console.log( this._data );
     return this._data;
 };
 
@@ -30,7 +30,7 @@ Heap.prototype.peek = function() {
 
 };
 
-Heap.prototype.insert = function(element) {
+Heap.prototype.insert = function( element ) {
 
 };
 
